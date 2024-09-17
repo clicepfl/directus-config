d0577bfc7cd63e4d6f6feb42f38731a43af2df04
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-09-17 11:11:48 +0200

    fix: correctly set basedir in scripts

--- populate.sh
+++ populate.sh
@@ -1,4 +1,6 @@
-docker exec -i clic-postgres psql --user directus_user directus_data < ./directus/dump.sql
+BASEDIR=$(dirname "$0")
+
+docker exec -i clic-postgres psql --user directus_user directus_data < $BASEDIR/dump.sql
 
 # Restarts directus to reload the permission table
 docker restart clic-directus
--- save.sh
+++ save.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+BASEDIR=$(dirname "$0")
 
 echo "[INFO] Creating data model snapshot"
 
@@ -7,9 +8,9 @@ docker exec -it clic-directus npx directus schema snapshot /share/snapshot.yaml 
 echo "[INFO] Generating typescript declarations"
 
 docker exec -u root -it clic-directus npx directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://127.0.0.1:8055 -o /share/schema.d.ts
-mv directus/schema.d.ts app/src/types/schema.d.ts
+mv $BASEDIR/schema.d.ts app/src/types/schema.d.ts
 
 # Hard fix for singleton elements
 sed -i -e 's/association: components\["schemas"\]\["ItemsAssociation"\]\[\]/association: components\["schemas"\]\["ItemsAssociation"\]/' app/src/types/schema.d.ts
 
-npx prettier --write app/src/types/schema.d.ts directus/snapshot.yaml
+npx prettier --write app/src/types/schema.d.ts $BASEDIR/snapshot.yaml
