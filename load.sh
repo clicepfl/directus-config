8612b174ea5963daf98a5fff069da914ad5f9253
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-08 16:40:34 +0200

    chore: populate flows in load.sh

--- load.sh
+++ load.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+BASEDIR=$(dirname "$0")
+
 docker exec -i clic-directus npx directus schema apply /share/snapshot.yaml -y
+docker exec -i clic-postgres psql --user directus_user directus_data < $BASEDIR/flows.sql
