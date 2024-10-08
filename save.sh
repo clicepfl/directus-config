65fb35e1e7b24b74bc1545ae18fca0b34e6758f3
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-08 16:30:16 +0200

    chore: add instructions to export flows on save

--- save.sh
+++ save.sh
@@ -9,6 +9,9 @@ echo "[INFO] Generating typescript declarations"
 
 docker exec -u root -it clic-directus npx directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://127.0.0.1:8055 -o /share/schema.d.ts
 
+echo 'DELETE FROM directus_flows; DELETE FROM directus_operations;' > $BASEDIR/flows.sql
+docker exec clic-postgres pg_dump -U directus_user directus_data -t directus_operations -t directus_flows --data-only --column-inserts --on-conflict-do-nothing --disable-triggers >>$BASEDIR/flows.sql
+
 # Hard fix for singleton elements
 sed -i -e 's/association: components\["schemas"\]\["ItemsAssociation"\]\[\]/association: components\["schemas"\]\["ItemsAssociation"\]/' $BASEDIR/schema.d.ts
 mv -f $BASEDIR/schema.d.ts $BASEDIR/types/schema.d.ts
