b16aafe3456252e11d875ff46a9dba6aefa85af7
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-22 15:38:18 +0200

    chore(dev): fail scripts when multiple containers are running

--- utils.sh
+++ utils.sh
@@ -2,8 +2,8 @@ find_container() {
     label=$1
     ids=$(docker ps --filter label=org.clic.type=$label -q)
 
-    if [ $(wc -w <<<"$ids") == 2 ]; then
-        echo "Multiple container running with label org.clic.type=$label: $ids"
+    if [ $(wc -w <<<"$ids") != 1 ]; then
+        echo "Multiple or no containers running with label org.clic.type=$label: $ids"
         echo "This script can only be used when a single container with this label is running"
         exit 1
     fi
