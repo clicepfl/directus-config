ac2e9661e82b81fc09b463a722d4c75bd5c40663
Noe Terrier <noeterrier@proton.me>
2024-11-03 18:50:06 +0100

    feat: add subsonic traduction info

--- save.sh
+++ save.sh
@@ -9,9 +9,9 @@ docker exec -it $(directus_container) npx directus schema snapshot /share/snapsh
 echo "[INFO] Generating typescript declarations"
 
 docker exec -u root -it $(directus_container) npx directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://127.0.0.1:8055 -o /share/schema.d.ts
-mv $BASEDIR/schema.d.ts types/schema.d.ts
+mv $BASEDIR/schema.d.ts $BASEDIR/types/schema.d.ts
 
 # Hard fix for singleton elements
-sed -i -e 's/association: components\["schemas"\]\["ItemsAssociation"\]\[\]/association: components\["schemas"\]\["ItemsAssociation"\]/' types/schema.d.ts
+sed -i -e 's/association: components\["schemas"\]\["ItemsAssociation"\]\[\]/association: components\["schemas"\]\["ItemsAssociation"\]/' $BASEDIR/types/schema.d.ts
 
-npx prettier --write types/schema.d.ts $BASEDIR/snapshot.yaml
+npx prettier --write $BASEDIR/types/schema.d.ts $BASEDIR/snapshot.yaml
