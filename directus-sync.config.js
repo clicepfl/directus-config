b08b0fc1f4e51695bf750f2f0adc2d63487b4483
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-10-19 17:58:14 +0200

    chore(dev): add directus-sync config

---
+++ directus-sync.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  directusUrl: "http://localhost/directus",
+  directusEmail: "clic@epfl.ch",
+  directusPassword: "1234",
+  sortJson: true,
+  collections: true,
+  split: true,
+};
