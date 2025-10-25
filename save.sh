1879052c2c03df6857230c693eda7d4d6bacc30e
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-10-25 16:12:47 +0200

    fix: correctly copy new schema from container

--- save.sh
+++ save.sh
@@ -6,8 +6,8 @@ pushd $BASEDIR
 npx directus-sync pull
 popd
 
-docker exec -u root -it $(directus_container) npx directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://127.0.0.1:8055 -o /share/schema.d.ts
-mv -f $BASEDIR/schema.d.ts $BASEDIR/types/schema.d.ts
+docker exec -u root -it $(directus_container) npx -y directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://127.0.0.1:8055 -o /share/schema.d.ts
+docker cp $(directus_container):/share/schema.d.ts $BASEDIR/types/schema.d.ts
 
 # Hard fix for singleton elements
 sed -i -e 's/association: components\["schemas"\]\["ItemsAssociation"\]\[\]/association: components\["schemas"\]\["ItemsAssociation"\]/' $BASEDIR/types/schema.d.ts
