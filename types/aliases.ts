eacee21ecc82816c91bdc7adb2d7e4408b1448de
Lucie Mermod <lucie.mermod@gmail.com>
2026-03-31 00:02:13 +0200

    style: add prettier check & ignore

--- types/aliases.ts
+++ types/aliases.ts
@@ -5,7 +5,7 @@ import { Schema, components } from "./schema";
 export type Association = components["schemas"]["ItemsAssociation"];
 export type Subsonic = components["schemas"]["ItemsSubsonic"];
 export type GameStar = components["schemas"]["ItemsGameStar"];
-export type SaveTheDate =  components["schemas"]["ItemsSavetheDate"];
+export type SaveTheDate = components["schemas"]["ItemsSavetheDate"];
 
 export type AssociationMembership = Schema["association_memberships"][0];
 export type AssociationPartner = Schema["association_partners"][0];
@@ -25,14 +25,15 @@ export type ICBD = Schema["icbd"][0];
 export type ICBDSpeaker = Schema["icbd_speakers"][0];
 export type ICBDActivity = Schema["icbd_activities"][0];
 export type ICBDPhd = Schema["icbd_phds"][0];
-export type ICBDSpeakerActivityRelation = Schema["icbd_activities_icbd_speakers"][0];
+export type ICBDSpeakerActivityRelation =
+  Schema["icbd_activities_icbd_speakers"][0];
 export type Registration = Schema["registrations"][0];
 export type Event = Schema["events"][0];
-export type ICBDActivityRegistration = Schema["icbd_activities_registrations"][0];
+export type ICBDActivityRegistration =
+  Schema["icbd_activities_registrations"][0];
 export type GameStarArticle = Schema["game_star_articles"][0];
 export type GameStarEvent = Schema["game_star_events"][0];
 export type GameStarProject = Schema["game_star_projects"][0];
 export type GameStarSocialLink = Schema["game_star_social_links"][0];
 export type Gallery = Schema["galleries"][0];
 export type Clothes = Schema["clothes"][0];
-
