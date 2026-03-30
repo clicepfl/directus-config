e643aaed1ae00b490fc28ba589400cb116222174
Lucie Mermod <lucie.mermod@gmail.com>
2026-03-30 23:59:27 +0200

    feat: add EventHandler type

--- hooks/types.ts
+++ hooks/types.ts
@@ -2,38 +2,54 @@ import { Schema } from "../types/schema";
 
 type Flatten<T> = T extends any[] ? T[number] : T;
 type IdOf<T> = T extends { id?: unknown } ? T["id"] : never;
+type EventType = "create" | "update" | "delete";
 
-export interface CreateEvent<K extends keyof Schema> {
+export type CreateEvent<K extends keyof Schema> = {
   event: `${K}.items.create`;
   payload: Flatten<Schema[K]>;
   key: IdOf<Flatten<Schema[K]>>;
   collection: K;
-}
+};
 
-export interface UpdateEvent<K extends keyof Schema> {
+export type UpdateEvent<K extends keyof Schema> = {
   event: `${K}.items.update`;
   payload: Flatten<Schema[K]>;
   key: string[];
   collection: K;
-}
+};
 
-export interface DeleteEvent<K extends keyof Schema> {
+export type DeleteEvent<K extends keyof Schema> = {
   event: `${K}.items.delete`;
   payload: string[];
   key: string[];
   collection: K;
+};
+
+export type Event<
+  K extends keyof Schema,
+  T extends EventType,
+> = T extends "create"
+  ? CreateEvent<K>
+  : T extends "update"
+    ? UpdateEvent<K>
+    : DeleteEvent<K>;
+
+export type EventHandler<K extends keyof Schema, T extends EventType> = (
+  event: Event<K, T>,
+) => void;
+
+function registerFlow<K extends keyof Schema, T extends EventType>(
+  name: `${K}.items.${T}`,
+  handler: EventHandler<K, T>,
+) {
+  // smth
 }
 
-export type Event<K extends keyof Schema> =
-  | CreateEvent<K>
-  | UpdateEvent<K>
-  | DeleteEvent<K>;
-
-function test(e: CreateEvent<"artists">) {
-  e.key;
-  e.payload;
-
-  foo(test)
+function myHandler(e: Event<"artists" | "association", "create" | "update">) {
+  console.log("smth happened");
 }
 
-function foo<K extends keyof Schema>(handler: (e: Event<K>) => void) {}
+registerFlow("artists.items.create", myHandler);
+registerFlow("artists.items.update", myHandler);
+registerFlow("association.items.create", myHandler);
+registerFlow("association.items.update", myHandler);
